99b4f8b535e494f3d8cca4dac0fbc0e1759e466f
Laerte Mercier <laertejjunior@gmail.com>
2023-05-10 01:37:18 -0300

    add calc function

--- js/calc.js
+++ js/calc.js
@@ -1,4 +1,6 @@
 const form = document.querySelector('#form-calculadora');
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
