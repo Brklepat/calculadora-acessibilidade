872b3b951760a6cf9f92dc41bf7ccad9a16c8b51
Laerte Mercier <laertejjunior@gmail.com>
2023-05-10 09:24:43 -0300

    Add questions dynamically

--- js/calc.js
+++ js/calc.js
@@ -1,6 +1,4 @@
 const form = document.querySelector('#form-calculadora');
-const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
-const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
